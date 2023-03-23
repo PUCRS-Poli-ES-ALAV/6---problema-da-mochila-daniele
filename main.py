1d334cca444ccb7ecc6758becc058d7a7d8d8360
Daniele Paz <42788455+dragonqveen@users.noreply.github.com>
2023-03-23 22:01:44 +0000

    :sparkles: Add brute force alg

--- main.py
+++ main.py
@@ -14,3 +14,21 @@ def fib_rec(n):
         return a + b
 
 print(fib_rec(5))
+
+#FIBO (n)
+#     f [0] ← 0 
+# f [1] ← 1
+# para i ← 2 até n faça
+#        f[i] ← f[i-1]+f[i-2]
+# devolva f [n]
+
+fib = []
+
+def fib(n);
+    fib[0] = 0
+    fib[1] = 1
+
+    for i in range(2..n):
+        fib[i] = fib[i-1] + fib[i-2]
+
+    return f[n]
