dc9d9ae09d9c680840afe7c0ed24d09837137c52
Daniele Paz <42788455+dragonqveen@users.noreply.github.com>
2023-04-13 21:59:33 +0000

    :sparkles:

--- mochila.py
+++ mochila.py
@@ -1,62 +1,48 @@
-class Item:
-    peso = 0
-    valor = 0
 
-    def __init__(self, peso, valor):
-        self.peso = peso
-        self.valor = valor
-
-#----------------------------------------------#
-
-def organizar_bp(qtd_prod, cap_total, itens):
-    itens.insert(0, None)
+def organizar_bp(cap_total, pesos, valores):
+    qtd_prod = len(pesos)
     max_tab = [[0] * (cap_total + 1)] * (qtd_prod + 1)
-
-    #printa_tab(max_tab)
-
-    for i in range(1, qtd_prod):
-        for j in range(1, cap_total):
-            item_atual = itens[i]
-            
-            valor_mesma_mochila_sem_item = max_tab[i - 1][j]
-
-            if item_atual.peso <= j:
-                valor_mochila = max_tab[i - 1][j - item_atual.peso]
-                
-                soma_valores = item_atual.valor + valor_mochila
-                
-
-                max_tab[i][j] = max(valor_mesma_mochila_sem_item, soma_valores)
-                
+    
+    for i in range(qtd_prod):
+        for j in range(cap_total):
+            print("indice item atual: " + str(i))
+            #print(j)
+            if(pesos[i] > j):
+                print("peso item atual: " + str(pesos[i]))
+                max_tab[qtd_prod][cap_total] = max_tab[qtd_prod][cap_total-1]
             else:
-                max_tab[i][j] = max_tab[i - 1][j]
+                
+                print("anterior: "+ str(max_tab[qtd_prod][cap_total-1]))
+                print("anterior + valor: "+ str(max_tab[qtd_prod][cap_total-1] + valores[i]))
+                
+                max_tab[qtd_prod][cap_total] = max(max_tab[qtd_prod][cap_total-1], max_tab[qtd_prod][cap_total-1] + valores[i])
+                
+                print("valor escolhido:" + str(max(max_tab[qtd_prod][cap_total-1], max_tab[qtd_prod][cap_total-1] + valores[i])))
+                printa_tab(max_tab)
+                print()
+            
 
     printa_tab(max_tab)
 
-    return max_tab[qtd_prod][cap_total]
-    
-    
 def printa_tab(tab):
     for i in tab:
         print(i)
 
-
-def cria_itens(pesos, valores):
-    lista = []
-    for i in range(len(pesos)):
-        lista.append(Item(pesos[i], valores[i]))
-
+def gera_casos(caso):
+    if(caso == 1):
+        return [165, [23, 31, 29, 44, 53, 38, 63, 85, 89, 82], [92, 57, 49, 68, 60, 43, 67, 84, 87, 72]]
     
-    return lista
+    elif(caso == 2): 
+        return [190, [56, 59, 80, 64, 75, 17], [50, 50, 64, 46, 50, 5]]
+    elif(caso == 3):
+        return [2, [1, 1, 1], [10, 20, 30]]
+    elif(caso == 4):
+        return [50, [10, 20, 30], [60, 100, 120]]
+    else:
+        return [7, [5,2,2,1], [2,4,2,3]]
+    
 
-#valores_caso = [165, [23, 31, 29, 44, 53, 38, 63, 85, 89, 82], [92, 57, 49, 68, 60, 43, 67, 84, 87, 72]]
-valores_caso = [190, [56, 59, 80, 64, 75, 17], [50, 50, 64, 46, 50, 5]]
+caso = gera_casos(3)
 
-itens_caso = cria_itens(valores_caso[1], valores_caso[2])
+print(organizar_bp(caso[0], caso[1], caso[2]))
 
-#caso = [valores_caso[0], itens_caso]
-
-itens = [Item(5,2), Item(2,4), Item(2,2), Item(1,3)]
-caso = [7, itens]
-
-print(organizar_bp(len(caso[1]), caso[0], caso[1]))
